09b687a9725595450b570515bf4cd0f0c1331882
Juan Perez <juanmarcperez@gmail.com>
2018-04-04 23:51:53 -0500

    first commit, shell app

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import searchBar from './searchBar';
+import calendar from './calendar';
+
+
+class App extends Component {
+  render (){
+    return(
+      <div>
+      <searchBar/>
+    </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/searchBar.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class searchBar extends Component {
+  render() {
+   return (
+     <div>
+     Hello
+   </div>
+ );
+}
+}
+
+export default searchBar;
+
+
+//google search api key
+//AIzaSyCzi3aMp4aFKFnpizrwLLK2N8tzdOs3YDU
+
+//custom seach api
+//AIzaSyBvcgRHsfLOFM6x8OMia8xjrmZregKIfFg
